ccf6983c33cd5017cb4a94dbedb8cf39e18c84b9
orels1 <orel-s1@yandex.ru>
2016-05-03 03:13:02 +0300

    added userbars

--- drawing/drawing.py
+++ drawing/drawing.py
@@ -172,6 +172,134 @@ class Drawing:
         else:
             await self.bot.say("Error getting image")
 
+    @commands.command(pass_context=True)
+    async def userbar(self, ctx, alt = None):
+        """Generates a server-based userbar"""
+
+        # get an avatar
+        avatar_url = ctx.message.author.avatar_url
+        avatar_image = Image
+
+        #get server icon
+        server_url = ctx.message.server.icon_url
+        server_image = Image
+
+        # get images
+        try:
+            async with aiohttp.get(avatar_url) as r:
+                image = await r.content.read()
+            with open('data/drawing/temp_avatar','wb') as f:
+                f.write(image)
+                success = True
+        except Exception as e:
+            success = False
+            print(e)
+
+        try:
+            async with aiohttp.get(server_url) as r:
+                image = await r.content.read()
+            with open('data/drawing/temp_server','wb') as f:
+                f.write(image)
+                success = True
+        except Exception as e:
+            success = False
+            print(e)
+
+        if success:
+
+            # load images
+            avatar_image = Image.open('data/drawing/temp_avatar').convert('RGBA')
+            server_image = Image.open('data/drawing/temp_server').convert('RGBA')
+
+            # get a font
+            fnt = ImageFont.truetype('data/drawing/font.ttf', 25)
+            fnt_sm = ImageFont.truetype('data/drawing/font.ttf', 18)
+            fnt_b = ImageFont.truetype('data/drawing/font_bold.ttf', 25)
+
+            if alt is None:
+                # prepare canvas to work with
+                result = Image.new('RGBA', (400, 100), (0,0,0,255))
+                process = Image.new('RGBA', (400, 100), (0,0,0,255))
+
+                # get a drawing context
+                d = ImageDraw.Draw(process)
+
+                # haax
+                d.rectangle([(0,0), (400,100)])
+
+                # paste background (server icon) to final image 
+                server_image = server_image.resize(size=(100,100))               
+                result.paste(server_image, (0,0))
+
+                # draw filter
+                d.rectangle([(0,0),(400,100)], fill=(0,0,0,160))
+
+                # draw overlay
+                d.rectangle([(10,10),(390,90)], fill=(0,0,0,190), outline=(200,200,200,128))
+
+                 # paste avatar
+                avatar_image = avatar_image.resize(size=(60,60))
+                process.paste(avatar_image, (20,20))
+
+                # get role
+                roles = ctx.message.author.roles
+                if len(roles) == 1:
+                    role = "member"
+                else:
+                    max_role = max([r.position for r in roles])
+                    index = [r.position for r in roles].index(max_role)
+                    role = roles[index].name
+
+                # draw text
+                name_size = fnt_b.getsize(ctx.message.author.name)[0]
+                d.text((110, 30), ctx.message.author.name, font=fnt_b, fill=(255,255,255,255))
+                d.text((110 + name_size + 10, 33), "#" + ctx.message.author.discriminator, font=fnt_sm, fill=(255,255,255,128))
+                d.text((110, 60), "A proud " + role + " of " + ctx.message.server.name, font=fnt_sm, fill=(255,255,255,180))
+
+            else:
+                # prepare canvas to work with
+                result = Image.new('RGBA', (200,200), (0,0,0))
+                process = Image.new('RGBA', (200,200), (0,0,0))
+
+                # get a drawing context
+                d = ImageDraw.Draw(process)
+
+                # haax
+                d.rectangle([(0,0), (200,200)])
+
+                # paste background (server icon) to final image
+                server_image = server_image.resize(size=(200,200))
+                result.paste(server_image, (0,0))
+
+                # draw filter
+                d.rectangle([(0,0),(200,200)], fill=(0,0,0,160))
+
+                #draw overlay
+                d.rectangle([(10,10),(190,190)], fill=(0,0,0,190), outline=(200,200,200,128))
+
+                # paste avatar
+                avatar_image = avatar_image.resize(size=(100,100))
+                process.paste(avatar_image, (50,25))
+
+                # draw text
+                name_pos = int(math.floor(fnt_b.getsize(ctx.message.author.name)[0] / 2))
+                discr_pos = int(math.floor(fnt_sm.getsize("#"+ctx.message.author.discriminator)[0] / 2))
+                d.text((100 - name_pos, 140), ctx.message.author.name, font=fnt_b, fill=(255,255,255,255))
+                d.text((100 - discr_pos, 162), "#" + ctx.message.author.discriminator, font=fnt_sm, fill=(255,255,255,128))
+
+            result = Image.alpha_composite(result, process)
+
+            result.save('data/drawing/temp.jpg','JPEG', quality=100)
+
+            await self.bot.send_file(ctx.message.channel, 'data/drawing/temp.jpg')
+
+            os.remove('data/drawing/temp.jpg')
+            os.remove('data/drawing/temp_avatar')
+            os.remove('data/drawing/temp_server')
+
+        else:
+            await self.bot.say("Couldn't get images")
+
 def setup(bot):
     if pil_available is False:
         raise RuntimeError("You don't have Pillow installed, run\n```pip3 install pillow```And try again")
